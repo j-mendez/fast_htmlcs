34b42775bd16d8c6d2a6cb4771015c07000297a5
j-mendez <jeff@a11ywatch.com>
2022-06-10 05:37:57 -0400

    chore(dups): fix detecting duplicate elements

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var fs         = require('fs');
-var scriptPath = __dirname+'/build/HTMLCS.js';
+var fs = require("fs");
+var scriptPath = __dirname + "/build/HTMLCS.js";
 
 if (fs.existsSync(scriptPath) === true) {
-    module.exports = require(scriptPath);
+  module.exports = require(scriptPath);
 } else {
-    throw new Error('HTMLCS must be built using `grunt build` before it can be required in Node.js.');
+  throw new Error(
+    "HTMLCS must be built using `grunt build` before it can be required in Node.js."
+  );
 }
